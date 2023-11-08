f8eae9c970a15b4c7815c0b8c359d7f2b96e7e65
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-07 22:31:14 -0800

    Practiced Generator function

---
+++ generatorFunction.js
@@ -0,0 +1,15 @@
+function* iteratorFunc() {
+    let count = 0;
+    for (let i = 0; i < 2; i++) {
+        count++;
+        yield i;
+    }
+    return count;
+  }
+  
+  let iterator = iteratorFunc();
+
+  console.log(iterator)
+  console.log(iterator.next()); 
+  console.log(iterator.next()); 
+  console.log(iterator.next());
